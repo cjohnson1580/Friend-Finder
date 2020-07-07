9692e76cc877c90421e1e0a14f0d8c57596357e7
cjohnson1580 <60634139+cjohnson1580@users.noreply.github.com>
2020-07-07 16:33:17 -0500

    Add files via upload

---
+++ friends.js
@@ -0,0 +1,21 @@
+var friendsData = [
+    {
+      name: "Iron Man",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQritLTZNaj1mZHMSmLwroZb4tvKaAs-E81eA&usqp=CAU",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Thor",
+      photo:"https://cdn.britannica.com/73/182873-050-E1C686F4/Chris-Hemsworth-Thor-Thor-The-Dark-World.jpg",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Captain America",
+      photo:"https://www.goliath.com/wp-content/uploads/2016/05/Captain-America.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    ];
+
+
+    // Export to API Route
+module.exports = friendsData;
